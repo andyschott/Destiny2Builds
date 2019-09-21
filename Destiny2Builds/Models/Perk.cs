a2fd86eac782bc50b4dc3e2b8b8c75768051edcf
Andy Schott <andy@andyschott.com>
2019-09-21 10:30:55 -0400

    render perk icons correctly

--- Destiny2Builds/Models/Perk.cs
+++ Destiny2Builds/Models/Perk.cs
@@ -15,8 +15,25 @@ namespace Destiny2Builds.Models
             Categories = categories;
         }
 
+        private static ISet<uint> _modCategories = new HashSet<uint>
+        {
+            4104513227, // Armor Mods
+            56,         // Solstice Armor Glows (I hope)
+        };
+
         public bool IsSelected { get; set; }
         public uint CategoryHash { get; }
         public IEnumerable<DestinyItemCategoryDefinition> Categories { get; }
+
+        // TODO: Might need to update this for weapon mods if that is ever needed.
+        public bool IsMod
+        {
+            get
+            {
+                var categoryHashes = Categories.Select(category => category.Hash);
+                var intersection = _modCategories.Intersect(categoryHashes);
+                return intersection.Any();
+            }
+        }
     }
 }
